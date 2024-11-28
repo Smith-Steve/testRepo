5b92b31783653d119f2d3c7a5cf5e95b36ad71bf
Steve Smith <Steve.A.Dore@outlook.com>
2024-11-28 08:18:03 -0800

    Initial commit

---
+++ src/TestRepo/TestRepoStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace TestRepo
+{
+    public class TestRepoStack : Stack
+    {
+        internal TestRepoStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
